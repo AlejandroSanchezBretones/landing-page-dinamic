35c9fabaf76d78d065dcca593d2fbfd1ff3ae140
AlejandroSanchezBretones <sangchez@live.com>
2020-06-25 12:41:41 +0200

    php added

--- php/Formulario_correo.php
+++ php/Formulario_correo.php
@@ -1,5 +1,12 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
+
 
 <p> Thank you! Your email <?php echo $_POST["email"]; ?> has been saved! :D</p>
 
